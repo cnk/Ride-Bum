ba7b6471319c3f3a1acf2fdd7b2dfeb444a12472
Cynthia Kiser <cnk@ugcs.caltech.edu>
2012-04-03 20:10:24 -0700

    try to set this up for token auth for invitees and passwords for event planners

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,26 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  # :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :token_authenticatable, :registerable,
     :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :login, :username, :email, :password, :password_confirmation, :remember_me, :phone
   attr_accessor :login
+
+  # For invitees, we need a token they can authenticate with. Let's add that globally.
+  before_save :ensure_authentication_token
+
+  has_many :invitations
+
+  scope :invitees, joins(:invitations)
+
+  # Sadly, we can't use this while creating a user to see if we need a
+  # token or password because we need the user so we can create the invitation
+  def is_an_invitee?
+    self.invitations.size > 0
+  end
+
   protected
   def self.find_for_database_authentication(warden_conditions)
     conditions = warden_conditions.dup
