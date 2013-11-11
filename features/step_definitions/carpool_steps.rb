81191b79a75caea9738398203250c99b350e13bb
John S. Ryan <jtigger@infosysengr.com>
2013-11-11 07:28:04 -0800

    Added semantic CSS to Ride partial distinguishing driver from passengers.

--- features/step_definitions/carpool_steps.rb
+++ features/step_definitions/carpool_steps.rb
@@ -4,9 +4,10 @@ When /^he enters (\d+) for the number of free seats in his car$/ do |seats|
 end
 
 Then /^he should see his car on the list of rides$/ do
-  page.should have_css('ul#carpools li', :text => "Driver: Jeff")
+  page.should have_css('ul#carpools li.driver', :count => 1)
+  page.should have_css('ul#carpools li.driver', :text => 'Jeff')
 end
 
 Then /^he should see (\d+) empty slots for passengers$/ do |free_seats|
-  page.should have_css('ul#carpools li', :count => free_seats.to_i + 1)
+  page.should have_css('ul#carpools li.passenger', :count => free_seats.to_i)
 end
